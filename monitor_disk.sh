1ead95ae083c7bd9947600c23059be6242455ea8
Your Name <your-email@example.com>
2025-07-31 14:59:53 +0530

    FIXED DISK USAGE SCRIPT

--- monitor_disk.sh
+++ monitor_disk.sh
@@ -8,11 +8,11 @@ echo "Current disk usage : $USAGE%"
 # Check if USAGE is a valid number
 if [[ "$USAGE" =~ ^[0-9]+$ ]]; then
     if [ "$USAGE" -gt 80 ]; then
-        echo "⚠️  WARNING: Disk usage is above 80%!"
+        echo "WARNING: Disk usage is above 80%!"
     else
-        echo "✅ Disk usage is under control"
+        echo "Disk usage is under control"
     fi
 else
-    echo "❌ ERROR: Could not parse disk usage value"
+    echo " ERROR: Could not parse disk usage value"
 fi
 
